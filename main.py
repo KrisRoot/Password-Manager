751c11aac74acacd0fd2af4e91174124b1848b14
KrisRoot <151185620+KrisRoot@users.noreply.github.com>
2023-12-18 17:07:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-from PyQt5.QtCore import QRect, QSize
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt, QRect, QSize
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
 from PyQt5.QtWidgets import (
+    QTableView,
     QApplication,
     QMainWindow,
     QAbstractItemView,
+    QDialog,
     QListWidget,
     QWidget,
     QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
     QLabel,
     QLineEdit,
     QCheckBox,
     QPushButton,
+    QListView,
     QMessageBox,
 )
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+from pyDes import *
 import sqlite3
 import qdarkstyle
 import hashlib
+import binascii
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+import binascii, os
+import random, string
 import base64
+
 
 THEME = 'light'
 
@@ -39,14 +49,12 @@ def change_theme(theme):
         f.write(theme + '\n')
         f.write(lines[1])
 
-
 def encrypt_text(text, key):
     key = key * 2
     cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
     padded_text = pad(text.encode('utf-8'), AES.block_size)
     encrypted_text = cipher.encrypt(padded_text)
     return base64.b64encode(encrypted_text).decode('utf-8')
-
 
 def decrypt_text(encrypted_text, key):
     key = key * 2
@@ -79,10 +87,10 @@ def get_data(_id):
     sql_query = f"SELECT * FROM database WHERE id = {_id};"
     cursor.execute(sql_query)
     results = cursor.fetchall()
+    return results[0]
 
     cursor.close()
     conn.close()
-    return results[0]
 
 
 def delete_data(_id):
@@ -196,19 +204,19 @@ class MainWindow(QMainWindow):
         self.pushbutton.clicked.connect(self.save_changes)
 
         self.pushbutton_3 = QPushButton("", self.central_widget)
-        self.pushbutton_3.setGeometry(QRect(0, 0, 51, 51))
+        self.pushbutton_3.setGeometry(QRect(0, 0, 50, 50))
         self.pushbutton_3.clicked.connect(self.add_key_mode)
 
         self.pushbutton_4 = QPushButton("", self.central_widget)
-        self.pushbutton_4.setGeometry(QRect(50, 0, 51, 51))
+        self.pushbutton_4.setGeometry(QRect(50, 0, 50, 50))
         self.pushbutton_4.clicked.connect(self.delete_key)
 
         self.pushbutton_5 = QPushButton("", self.central_widget)
-        self.pushbutton_5.setGeometry(QRect(100, 0, 51, 51))
+        self.pushbutton_5.setGeometry(QRect(100, 0, 50, 50))
         self.pushbutton_5.clicked.connect(self.open_settings)
 
         self.pushbutton_6 = QPushButton("", self.central_widget)
-        self.pushbutton_6.setGeometry(QRect(150, 0, 51, 51))
+        self.pushbutton_6.setGeometry(QRect(150, 0, 50, 50))
         self.pushbutton_6.clicked.connect(self.open_about)
 
         self.listview = QListWidget(self.central_widget)
@@ -226,6 +234,14 @@ class MainWindow(QMainWindow):
         self.listview_3.setGeometry(QRect(415, 50, 189, 439))
         self.listview_3.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.listview_3.doubleClicked.connect(self.list_selected)
+
+        self.listview.verticalScrollBar().valueChanged.connect(self.sync_scrollbars)
+        self.listview_2.verticalScrollBar().valueChanged.connect(self.sync_scrollbars)
+        self.listview_3.verticalScrollBar().valueChanged.connect(self.sync_scrollbars)
+
+        self.listview.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.listview_2.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.listview_3.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 
         if THEME == 'light':
             self.pushbutton_3.setIcon(QIcon('img/add.png'))
@@ -265,6 +281,11 @@ class MainWindow(QMainWindow):
                 except ValueError:
                     self.listview_3.addItems([data[2]])
                 self.listview_2.addItems([data[3]])
+
+    def sync_scrollbars(self, value):
+        self.listview.verticalScrollBar().setValue(value)
+        self.listview_2.verticalScrollBar().setValue(value)
+        self.listview_3.verticalScrollBar().setValue(value)
 
     def check_password(self):
         file = open("settings.txt", "r")
@@ -326,6 +347,16 @@ class MainWindow(QMainWindow):
         self.mode = "change"
         self.pushbutton.setText("Change Value")
 
+        id_select = self.listview.currentRow()
+        if id_select == -1:
+            id_select = self.listview_2.currentRow()
+            if id_select == -1:
+                id_select = self.listview_3.currentRow()
+
+        self.lineedit.setText(self.listview.item(id_select).text())
+        self.lineedit_2.setText(self.listview_3.item(id_select).text())
+        self.lineedit_3.setText(self.listview_2.item(id_select).text())
+
     def open_settings(self):
         msg = QMessageBox()
         msg.setText("Choose th theme")
@@ -344,7 +375,7 @@ To change values double click on the values which you want to edit and then pres
 To delete values double click on the values which you want to delete and then press \"Delete\" button\n
 To open settings press \"Settings\" button\n
 To get help press \"Help\" button\n
-Program made special for Corporation of evil\nMade on PyQT5\nSomeOne""")
+Program made special for Yandex Lyceum\nMade on PyQT5\nYaroslav Demidov""")
         msg.setWindowTitle("About")
         msg_no = msg.addButton("Ok", msg.AcceptRole)
         msg.exec_()
@@ -379,8 +410,8 @@ Program made special for Corporation of evil\nMade on PyQT5\nSomeOne""")
             elif self.mode == "change":
                 id_select = -1
                 name = self.lineedit.text()
-                password = self.lineedit_3.text()
-                description = self.lineedit_2.text()
+                password = self.lineedit_2.text()
+                description = self.lineedit_3.text()
                 con = sqlite3.connect("database.sqlite")
                 cur = con.cursor()
                 id_select = self.listview.currentRow()
@@ -400,8 +431,8 @@ Program made special for Corporation of evil\nMade on PyQT5\nSomeOne""")
                 for i in range(1, last_id):
                     data = get_data(i)
                     self.listview.addItems([data[1]])
-                    self.listview_2.addItems([decrypt_text(data[2], self.setup_password)])
-                    self.listview_3.addItems([data[3]])
+                    self.listview_3.addItems([decrypt_text(data[2], self.setup_password)])
+                    self.listview_2.addItems([data[3]])
 
 
 if __name__ == "__main__":
